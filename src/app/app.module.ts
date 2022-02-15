a39b2f2c59b231c99440ef4db16316f1a552c622
Marisu <lic.msferro.vla@gmail.com>
2022-02-15 20:06:15 -0300

    sección home: portada y sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PortadaComponent } from './body/home/portada/portada.component';
+import { SidebarComponent } from './body/home/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PortadaComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
