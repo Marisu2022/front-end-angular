f508cc7285edb641dfc66a0f8f7097c8f9ec39ee
Marisu <lic.msferro.vla@gmail.com>
2022-02-25 21:10:05 -0300

    agregué sección educación, proyectos  y habilidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,16 @@ import { PortadaComponent } from './body/home/portada/portada.component';
 import { SidebarComponent } from './body/home/sidebar/sidebar.component';
 import { AboutComponent } from './body/about/about.component';
 import { TableComponent } from './body/experience/table/table.component';
-import { OthersComponent } from './body/experience/others/others.component';
 import { TitleComponent } from './body/experience/title/title.component';
+import { EducationComponent } from './body/education/education.component';
+import { TableStudiesComponent } from './body/education/table-studies/table-studies.component';
+import { CoursesComponent } from './body/education/courses/courses.component';
+import { OthersComponent } from './body/experience/others/others.component';
+import { SkillsComponent } from './body/skills/skills.component';
+import { SkillComponent } from './body/skills/skill/skill.component';
+import { ProjectsComponent } from './body/projects/projects.component';
+import { ProjectComponent } from './body/projects/project/project.component';
+
 
 
 
@@ -22,8 +30,16 @@ import { TitleComponent } from './body/experience/title/title.component';
     SidebarComponent,
     AboutComponent,
     TableComponent,
-    OthersComponent,
     TitleComponent,
+    EducationComponent,
+    TableStudiesComponent,
+    CoursesComponent,
+    OthersComponent,
+    SkillsComponent,
+    SkillComponent,
+    ProjectsComponent,
+    ProjectComponent,
+
 ],
   imports: [
     BrowserModule,
---
+++ src/app/body/education/table-studies/table-studies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-studies',
+  templateUrl: './table-studies.component.html',
+  styleUrls: ['./table-studies.component.css']
+})
+export class TableStudiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
