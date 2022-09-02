91158068c5be3af272f125414a6456b9e8a85281
Marisu <lic.msferro.vla@gmail.com>
2022-09-01 22:00:30 -0300

    revierto corchete

--- src/app/servicios/about.service.ts
+++ src/app/servicios/about.service.ts
@@ -8,11 +8,11 @@ import { About } from '../models/about';
   providedIn: 'root',
 })
 export class AboutService {
-  private backendUrl: String = environment.apiBaseUrl;
+  private backendUrl:String = environment.apiBaseUrl;
   
   constructor(private httpClient: HttpClient) {}
   public getabout(): Observable<About[]> {
-    return this.httpClient.get<[About]>(`${this.backendUrl}ver/about`);
+    return this.httpClient.get<About[]>(`${this.backendUrl}ver/about`);
   }
   public postnewabout(about: About): Observable<any> {
     return this.httpClient.post<any>(`${this.backendUrl}new/about`, about);
